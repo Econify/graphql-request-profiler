8bb5f72b472f2d4d6d372b8e24d9e53c1604c51a
Regan Karlewicz <regan@karlewr.net>
2022-05-23 20:17:41 -0700

    D3 demo

--- viz/chart.js
+++ viz/chart.js
@@ -244,49 +244,45 @@ var data = [
     location: 'page.zoneSets.1.zones.0.widgets.2.items.0.authors.0.image',
     parentType: 'Author',
   },
-  {
-    execStartTimeMs: 277,
-    execEndTimeMs: 2843,
-    execTimeMs: 2289,
-    location: 'ello.govna',
-    parentType: 'Author',
-  },
-].map((item) => {
-  return {
-    name: item.location,
-    end: item.execEndTimeMs,
-    count: item.execTimeMs,
-  };
-});
+];
 
-function HorizontalWaterfallChart(attachTo, data) {
-  const maxData = Math.max(...data.map((d) => d.end));
+function waterfall(attachTo, data) {
+  const maxData = Math.max(...data.map((d) => d.execEndTimeMs));
   const svgHeight = 2000;
-  const svgWidth = 1500;
+  const svgWidth = 1300;
   const numTicks = 10;
-  const recWidth = d3.min([50, svgHeight / data.length]); // Minimum width of the rect 50px;
+  const barHeight = d3.min([50, svgHeight / data.length]);
   const leftMargin = 160;
-  const lineHeight = data.length * recWidth;
+  const lineHeight = data.length * barHeight;
   const colorScale = d3.scale.category10();
+
+  var tooltip = d3
+    .select(attachTo)
+    .append('div')
+    .style('position', 'absolute')
+    .style('opacity', 0)
+    .style('width', '400px')
+    .style('background-color', 'black')
+    .style('color', 'white');
 
   // Append the chart and pad it a bit
   var chart = d3
     .select(attachTo)
     .append('svg')
     .attr('class', 'chart')
-    .attr('width', svgWidth)
+    .attr('width', svgWidth + leftMargin)
     .attr('height', svgHeight);
 
   // Set the x-axis scale
   var x = d3.scale
     .linear()
-    .domain([0, maxData + 10])
-    .range(['0px', `${svgWidth}px`]);
+    .domain([0, maxData + 20])
+    .range(['0px', `${svgWidth - leftMargin}px`]);
 
   // X-axis Label
   chart
     .append('g')
-    .attr('transform', 'translate(' + svgWidth * 0.6 + ',15)')
+    .attr('transform', 'translate(' + svgWidth * 0.5 + ',15)')
     .attr('class', 'gAxisLabel')
     .append('text')
     .text('Time (ms)');
@@ -307,46 +303,56 @@ function HorizontalWaterfallChart(attachTo, data) {
     .append('rect')
     .attr('class', 'rectWF')
     .attr('x', function (d, i) {
-      return x(d.end - d.count);
+      return x(d.execEndTimeMs - (d.execEndTimeMs - d.execStartTimeMs));
     })
     .attr('y', function (d, i) {
-      return i * recWidth;
+      return i * barHeight;
     })
     .style('fill', function (d, i) {
       return colorScale(i);
     })
-    .attr('height', recWidth)
-    .attr('width', 0)
-    // .on('mouseover', function () {
-    //   d3.selectAll('.rectWF').style('opacity', 0.2);
-    //   d3.select(this).style('opacity', 1);
-    // })
-    // .on('mouseout', function () {
-    //   d3.selectAll('.rectWF').style('opacity', 1);
-    // })
-    .transition()
-    .duration(1000)
+    .attr('height', barHeight)
+    .on('mouseover', function (d) {
+      d3.selectAll('.rectWF').style('opacity', 0.2);
+      d3.select(this).style('opacity', 1);
+      return tooltip.style('opacity', 1).text(JSON.stringify(d, null, 2));
+    })
+    .on('mousemove', function () {
+      return tooltip
+        .style('opacity', 1)
+        .style('top', d3.event.pageY + 20 + 'px')
+        .style('left', d3.event.pageX + 'px');
+    })
+    .on('mouseout', function () {
+      d3.selectAll('.rectWF').style('opacity', 1);
+      return tooltip.style('opacity', 0);
+    })
+    // .transition()
+    // .duration(1000)
     .attr('width', function (d, i) {
-      return x(d.count);
+      return x(d.execEndTimeMs - d.execStartTimeMs);
     });
 
   // Set the values on the bars
   chart
     .append('g')
     .attr('transform', 'translate(' + leftMargin + ',15)')
-    .selectAll('text')
+    .selectAll('rect')
     .data(data)
     .enter()
     .append('text')
     .attr('class', 'bar')
+    .attr('text-anchor', 'middle')
+    .attr('alignment-baseline', 'middle')
     .attr('x', function (d) {
-      return x(d.end - d.count / 2);
+      return x(d.execEndTimeMs - (d.execEndTimeMs - d.execStartTimeMs) / 2);
     })
     .attr('y', function (d, i) {
-      return i * recWidth + recWidth * 0.5;
+      return i * barHeight + barHeight * 0.5;
     })
+    .style('pointer-events', 'none')
     .text(function (d) {
-      return `${d.count}ms`;
+      return `${d.execEndTimeMs - d.execStartTimeMs}ms`;
     });
 
   // Set the vertical lines for axis
@@ -397,14 +403,15 @@ function HorizontalWaterfallChart(attachTo, data) {
     .append('text')
     .attr('x', leftMargin - 10)
     .attr('y', function (d, i) {
-      return i * recWidth + recWidth * 0.8;
+      return i * barHeight + barHeight * 0.8;
     })
-    .attr('dx', -5) // padding-right
-    .attr('dy', '0') // vertical-align: middle
-    .attr('text-anchor', 'end') // text-align: right
-    .text(function (d, i) {
-      return d.name;
+    .attr('dx', -5)
+    .attr('dy', '0')
+    .attr('text-anchor', 'end')
+    .attr('alignment-baseline', 'middle')
+    .text(function (d) {
+      return d.location;
     });
 }
 
-HorizontalWaterfallChart('body', data);
+waterfall('body', data);
