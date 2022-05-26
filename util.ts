06392802dba551a579cffcf1fcadb541c0672742
Regan Karlewicz <regan@econify.com>
2022-05-25 18:08:10 -0700

    Clean up into utils

--- util.ts
+++ util.ts
@@ -1,7 +1,16 @@
+import type { AxiosError } from 'axios';
 import type { GraphQLField, GraphQLSchema } from 'graphql';
-import type { GraphQLNamedType, TraceFunction } from './types';
+import type {
+  GraphQLNamedType,
+  IGraphQLRequestData,
+  IOptionData,
+  TraceFunction,
+} from './types';
 
 import { isIntrospectionType } from 'graphql';
+import childProcess from 'child_process';
+import axios from 'axios';
+import fs from 'fs';
 
 export const nsToMs = (nanoseconds: bigint) => {
   return Number(nanoseconds / BigInt(1000000));
@@ -30,4 +39,67 @@ function applyResolverToType(type: GraphQLNamedType, fn: TraceFunction) {
       }
     }
   }
+}
+
+function getOpenCommand() {
+  switch (process.platform) {
+    case 'darwin':
+      return 'open';
+    case 'win32':
+      return 'start';
+    default:
+      return 'xdg-open';
+  }
+}
+
+export function openUrl(url: string) {
+  const openCmd = getOpenCommand();
+  childProcess.exec(`${openCmd} ${url}`);
+}
+
+export async function requestGraphQL(
+  data: IGraphQLRequestData,
+  options: IOptionData
+) {
+  try {
+    return axios({
+      method: 'POST',
+      url: options.endpoint,
+      data,
+      headers: {
+        'x-trace': 'true',
+      },
+    });
+  } catch (e) {
+    const error = <AxiosError>e;
+
+    if (error?.response?.data) {
+      console.error(error.response.data);
+    } else {
+      console.error(error.message);
+    }
+
+    throw e;
+  }
+}
+
+function parseVariables(data: IGraphQLRequestData, options: IOptionData) {
+  const variables = fs.readFileSync(options.variables).toString();
+  data.variables = JSON.parse(variables);
+}
+
+export function getRequestBody(options: IOptionData) {
+  const data: IGraphQLRequestData = {
+    query: fs.readFileSync(options.schema).toString(),
+  };
+
+  if (options.operationName) {
+    data.operationName = options.operationName;
+  }
+
+  if (options.variables) {
+    parseVariables(data, options);
+  }
+
+  return data;
 }
--- visualize.ts
+++ visualize.ts
@@ -1,71 +1,28 @@
 #!/usr/bin/env node
 
 import commandLineArgs from 'command-line-args';
-import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
-import childProcess from 'child_process';
 import { IGraphQLRequestData, IOptionData } from './types';
 import { helpText } from './help';
-
-function getOpenCommand() {
-  switch (process.platform) {
-    case 'darwin':
-      return 'open';
-    case 'win32':
-      return 'start';
-    default:
-      return 'xdg-open';
-  }
-}
-
-function openUrl(url: string) {
-  const openCmd = getOpenCommand();
-  childProcess.exec(`${openCmd} ${url}`);
-}
+import { getRequestBody, openUrl, requestGraphQL } from './util';
 
 async function makeRequestAndOpenData(options: IOptionData) {
-  const data: IGraphQLRequestData = {
-    query: fs.readFileSync(options.schema).toString(),
-  };
+  const response = await requestGraphQL(getRequestBody(options), options);
 
-  if (options.operationName) {
-    data.operationName = options.operationName;
+  if (response.data.extensions.traces) {
+    console.error('Error: No traces found, is the plugin installed properly?');
+    process.exit(1);
   }
 
-  if (options.variables) {
-    // TODO: test me
-    const variables = fs.readFileSync(options.variables).toString();
-    data.variables = JSON.parse(variables);
-  }
+  // TODO: allow custom output file name in options, use tmp if doesn't exist
+  const tmpFileName = `tmp-${Math.random().toString().replace('.', '')}.json`;
+  fs.writeFileSync(
+    tmpFileName,
+    JSON.stringify(response.data.extensions.traces)
+  );
 
-  try {
-    const response = await axios({
-      method: 'POST',
-      url: options.endpoint,
-      data,
-      headers: {
-        'x-trace': 'true',
-      },
-    });
-
-    // TODO: print error if no extensions.traces field
-    // msg: "No traces found, is the plugin installed properly?"
-
-    // TODO: allow custom output file name in options, use tmp if doesn't exist
-    const tmpFileName = `tmp-${Math.random().toString().replace('.', '')}.json`;
-    fs.writeFileSync(
-      tmpFileName,
-      JSON.stringify(response.data.extensions.traces)
-    );
-
-    openData({ data: tmpFileName } as IOptionData);
-  } catch (e) {
-    console.error((e as Error).message);
-    // TODO: not every error has response, only show if it exists
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    console.error((e as any).response.data);
-  }
+  openData({ data: tmpFileName } as IOptionData);
 }
 
 function printHelp() {
